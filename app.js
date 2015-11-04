b20c402679253d2dbbc41f817e6112c8801d1db8
Patrix252 <patrix252@gmail.com>
2015-11-04 15:21:10 +0100

    memory 0.9

--- app.js
+++ app.js
@@ -5,14 +5,51 @@
 // done: when the user has discovered the pair of cards
 // hidden: when the card has not been discovered yet
 
-$(document).ready(function(){
+var counter = 1;
 
-  
+$(document).ready(function () {
+  var cards = $('.cards').children();
+  var shuffleData = generateData(10);
+
+  initializeGame(cards, shuffleData);
+
+
+
+  $(cards).click(function () {
+    var id = $(this).attr('id');
+
+    if (shuffleData[id] == counter) {
+      $(this).attr('class', 'done');
+      counter++;
+      if(counter == 11) {
+        alert('you win');
+        initializeGame(cards, shuffleData);
+      }
+    } else {
+      alert('you lose');
+    }
+
+  });
+
+  $('.opt-start').click(function() {
+    $(cards).attr('class', 'hidden');
+    counter = 1;
+  })
+
 });
 
 
 
 
 
+function initializeGame(cards, shuffleData) {
+  counter = 1;
 
+  $(cards).each( function(index) {
+    var id = $(this).attr('id');
+    var text = $(this).find('h3');
+    $(text).text(shuffleData[id]);
 
+    $(this).attr('class', 'done');
+  });
+}
